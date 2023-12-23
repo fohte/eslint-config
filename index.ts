29b32092aa0620be95aaf9c652225d0fd21c8daa
Hayato Kawai <fohte.hk@gmail.com>
2023-12-23 14:56:09 +0900

    add eslint configs

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+module.exports = {
+  plugins: ['simple-import-sort', 'import', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+}
